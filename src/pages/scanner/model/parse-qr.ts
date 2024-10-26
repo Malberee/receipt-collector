2030eddd183bae322148ef60584d68ad56c60c9b
Malberee <82259650+Malberee@users.noreply.github.com>
2024-10-26 23:13:17 +0300

    fix: improve QR code parsing

--- src/pages/scanner/model/parse-qr.ts
+++ src/pages/scanner/model/parse-qr.ts
@@ -4,9 +4,6 @@ import type { ReceiptType } from '@entities/receipt'
 
 import { getRarity } from './get-rarity'
 
-const regexURL =
-  /https:\/\/cabinet\.tax\.gov\.ua\/cashregs\/check\?mac=[a-f0-9]{64}&date=\d{8}&time=\d{2}:\d{2}:\d{2}&fn=\d+&id=[A-Za-z0-9]+&sm=\d+\.\d{2}/
-
 const parseURL = (data: string) => {
   const url = new URL(data)
 
@@ -25,12 +22,39 @@ const parseURL = (data: string) => {
 }
 
 export const parseQR = (data: string): Omit<ReceiptType, 'id' | 'products'> => {
+  const regexURL = new RegExp(
+    /https:\/\/cabinet\.tax\.gov\.ua\/cashregs\/check\?mac=[a-f0-9]{64}&date=\d{8}&time=\d{2}:\d{2}:\d{2}&fn=\d+&id=[A-Za-z0-9]+&sm=\d+\.\d{2}/,
+  )
+
+  const dateRegex = new RegExp(/\b\d{2}[\.-]\d{2}[\.-]\d{4}\b/)
+  const dateRegex2 = new RegExp(/\b\d{2}[\.-]\d{2}[\.-]\d{2}\b/)
+  const timeRegex = new RegExp(/\b\d{2}:\d{2}:\d{2}\b/)
+  const amountRegex = new RegExp(/\b=?\d+\.\d{2}\b(?!\.)/)
+  const fiscalNumberRegex = new RegExp(/\bF?N?\d{10}\b/)
+
   try {
     if (regexURL.test(data)) {
       return parseURL(data)
     }
 
-    const [_fiscalNumber, , _amount, _date, time] = data.split(' ')
+    const getFormattedDate = (string?: string) => {
+      return string?.split('').toSpliced(-2, -2, '20').join('')
+    }
+
+    const values = ([] as string[]).concat(
+      data.match(fiscalNumberRegex) ?? [],
+      data.match(amountRegex) ?? [],
+      getFormattedDate(data.match(dateRegex2)?.[0]) ??
+        data.match(dateRegex) ??
+        [],
+      data.match(timeRegex) ?? [],
+    )
+
+    if (values.length !== 4) {
+      throw 'Cannot scan this QR code!'
+    }
+
+    const [_fiscalNumber, _amount, _date, time] = values
 
     const amount = parseFloat(_amount.replace('=', ''))
     const date = moment(_date + time, 'DD.MM.YYYY HH:mm:ss').toDate()
