0ff2ad147030fb499683a64aa87105b5c6e834bb
Malberee <82259650+Malberee@users.noreply.github.com>
2024-10-27 16:08:05 +0200

    feat: add border to scanner

--- src/pages/scanner/ui/overlay.tsx
+++ src/pages/scanner/ui/overlay.tsx
@@ -1,6 +1,6 @@
 import { Canvas, DiffRect, rect, rrect } from '@shopify/react-native-skia'
 import React from 'react'
-import { Dimensions, Platform, StyleSheet } from 'react-native'
+import { Dimensions, Platform, StyleSheet, View } from 'react-native'
 
 const { width, height } = Dimensions.get('window')
 
@@ -14,18 +14,31 @@ const inner = rrect(
     innerDimension,
     innerDimension,
   ),
-  50,
-  50,
+  43,
+  43,
 )
 
 export function Overlay() {
   return (
-    <Canvas
-      style={
-        Platform.OS === 'android' ? { flex: 1 } : StyleSheet.absoluteFillObject
-      }
-    >
-      <DiffRect inner={inner} outer={outer} color="black" opacity={0.5} />
-    </Canvas>
+    <>
+      <Canvas
+        style={
+          Platform.OS === 'android'
+            ? { flex: 1 }
+            : StyleSheet.absoluteFillObject
+        }
+      >
+        <DiffRect inner={inner} outer={outer} color="black" opacity={0.5} />
+      </Canvas>
+      <View
+        className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 scale-110"
+        style={{ width: inner.rect.width, height: inner.rect.height }}
+      >
+        <View className="absolute left-0 top-0 size-[95px] rounded-tl-[50px] border-0 border-l-3 border-t-3 border-white" />
+        <View className="absolute right-0 top-0 size-[95px] rounded-tr-[50px] border-0 border-r-3 border-t-3 border-white" />
+        <View className="absolute bottom-0 left-0 size-[95px] rounded-bl-[50px] border-0 border-b-3 border-l-3 border-white" />
+        <View className="absolute bottom-0 right-0 size-[95px] rounded-br-[50px] border-0 border-b-3 border-r-3 border-white" />
+      </View>
+    </>
   )
 }
