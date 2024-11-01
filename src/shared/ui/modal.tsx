3106a0d01cb02cab3b613dce928f83d21cc9e504
Malberee <82259650+Malberee@users.noreply.github.com>
2024-11-01 18:48:25 +0200

    feat: implement modal

---
+++ src/shared/ui/modal.tsx
@@ -0,0 +1,45 @@
+import { Button, Plus } from '@malberee/nextui-native'
+import { cssInterop } from 'nativewind'
+import React, { type FC, type ReactNode } from 'react'
+import { type GestureResponderEvent, Pressable, View } from 'react-native'
+
+interface ModalProps {
+  children?: ReactNode
+  onClose?: (e: GestureResponderEvent) => void
+}
+
+cssInterop(Plus, {
+  className: {
+    target: 'style',
+    nativeStyleToProp: {
+      color: true,
+    },
+  },
+})
+
+export const Modal: FC<ModalProps> = ({ children, onClose }) => {
+  return (
+    <Pressable
+      onPress={onClose}
+      className="absolute z-20 h-screen w-screen flex-row items-center justify-center bg-black/40"
+    >
+      <View
+        onStartShouldSetResponder={() => true}
+        onTouchEnd={(e) => {
+          e.stopPropagation()
+        }}
+        className="relative rounded-medium bg-default-50 p-4"
+      >
+        <Button
+          startContent={<Plus className="rotate-45 text-foreground" />}
+          isIconOnly
+          variant="light"
+          color="default"
+          className="absolute right-2 top-2 z-10"
+          onPress={onClose}
+        />
+        {children}
+      </View>
+    </Pressable>
+  )
+}
