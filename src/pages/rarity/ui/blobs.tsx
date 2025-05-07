396424f2cdb6c1506478807fb47f8a17031aab88
Malberee <82259650+Malberee@users.noreply.github.com>
2025-05-07 23:51:05 +0300

    chore: replace svg blobs with skia and implement animation

--- src/pages/rarity/ui/blobs.tsx
+++ src/pages/rarity/ui/blobs.tsx
@@ -1,45 +1,79 @@
-import { cssInterop } from 'nativewind'
-import React, { type FC } from 'react'
-import Svg, { Path } from 'react-native-svg'
+import {
+  Canvas,
+  Path,
+  Skia,
+  usePathInterpolation,
+} from '@shopify/react-native-skia'
+import React, { type FC, useEffect } from 'react'
+import { type StyleProp, type ViewStyle } from 'react-native'
+import { Easing, useSharedValue, withTiming } from 'react-native-reanimated'
 
 interface BlobsProps {
   color: string
 }
 
-cssInterop(Svg, {
-  className: {
-    target: 'style',
-  },
-})
-
 export const Blobs: FC<BlobsProps> = ({ color }) => {
+  const progress = useSharedValue(0)
+
+  useEffect(() => {
+    progress.value = withTiming(progress.value ? 0 : 1, {
+      duration: 500,
+      easing: Easing.out(Easing.quad),
+    })
+  }, [])
+
+  const topPathStart = Skia.Path.MakeFromSVGString(
+    'M235.8 287.212C209.309 281.007 236.473 222.491 222.5 199C208.527 175.509 236.479 108.967 214.5 97C192.521 85.0328 219.906 36.9921 198 26C176.094 15.0079 109.576 53.799 105.5 21C101.424 -11.7103 6.84111 26.771 0 0H235.8V287.212Z',
+  )!
+  const topPathEnd = Skia.Path.MakeFromSVGString(
+    'M247 287.212C220.509 281.007 193.945 274.713 179.972 251.222C165.998 227.731 170.979 193.967 149 182C127.021 170.033 94.4061 160.492 72.5 149.5C50.5939 138.508 5.57556 113.299 1.50001 80.5C-2.57555 47.7897 18.0411 26.771 11.2 0H247V287.212Z',
+  )!
+
+  const topPath = usePathInterpolation(
+    progress,
+    [0, 1],
+    [topPathStart, topPathEnd],
+  )
+
+  const bottomPathStart = Skia.Path.MakeFromSVGString(
+    'M0 0C22.7794 19.2361 23.8482 53.4473 29.5 90C34.811 124.348 10.5 141 29.5 179C45 210 24.5511 238.661 29.5 261C34.4489 283.25 78 242.699 117 264C159.5 287.212 199.338 271.522 235.8 287.212H0V0Z',
+  )!
+  const bottomPathEnd = Skia.Path.MakeFromSVGString(
+    'M0 0.787476C22.7794 20.0236 21.3433 45.2708 37.5 63C53.5839 80.7292 69.4994 71.9153 96.5 77.5C123.573 82.996 161.051 117.161 166 139.5C170.949 161.75 161.057 191.199 161.931 223.909C162.877 256.619 199.338 272.31 235.8 288H0V0.787476Z',
+  )!
+
+  const bottomPath = usePathInterpolation(
+    progress,
+    [0, 1],
+    [bottomPathStart, bottomPathEnd],
+  )
+
+  const style: StyleProp<ViewStyle> = {
+    position: 'absolute',
+    width: 236,
+    height: 288,
+  }
+
   return (
     <>
-      <Svg
-        className="absolute right-0 top-0"
-        width="236"
-        height="288"
-        viewBox="0 0 236 288"
-        fill="none"
+      <Canvas
+        style={{
+          ...style,
+          top: 0,
+          right: 0,
+        }}
       >
-        <Path
-          d="M236 287.212C209.509 281.007 182.945 274.713 168.972 251.222C154.998 227.731 153.616 186.954 131.637 174.987C109.658 163.02 67.1555 179.774 45.2494 168.782C23.3433 157.79 22.0333 119.051 17.9578 86.2524C13.8822 53.5421 7.04111 26.771 0.199997 0H236V287.212Z"
-          fill={color}
-        />
-      </Svg>
-
-      <Svg
-        className="absolute bottom-0 left-0"
-        width="236"
-        height="288"
-        viewBox="0 0 236 288"
-        fill="none"
+        <Path path={topPath} color={color} />
+      </Canvas>
+      <Canvas
+        style={{
+          ...style,
+          bottom: 0,
+          left: 0,
+        }}
       >
-        <Path
-          d="M0 0.787476C22.7794 20.0236 45.4861 39.2598 61.6428 56.9889C77.7267 74.7181 87.1878 90.9403 114.188 96.525C141.262 102.021 185.802 96.8796 190.751 119.218C195.699 141.468 161.057 191.199 161.931 223.909C162.877 256.619 199.338 272.31 235.8 288H0V0.787476Z"
-          fill={color}
-        />
-      </Svg>
+        <Path path={bottomPath} color={color} />
+      </Canvas>
     </>
   )
 }
