4e5060060b6f0ec0bb90f5dd274f1795e07c2bc9
Malberee <82259650+Malberee@users.noreply.github.com>
2025-07-03 23:34:43 +0300

    chore(receipt-details): add list animations

--- src/pages/receipt-details/ui/receipt/index.tsx
+++ src/pages/receipt-details/ui/receipt/index.tsx
@@ -1,9 +1,11 @@
+import { observer } from 'mobx-react-lite'
 import { cssInterop } from 'nativewind'
 import React, { type FC, useCallback } from 'react'
 import { FlatList, type ListRenderItem, View } from 'react-native'
 import Dash from 'react-native-dashed-line'
+import Animated, { LinearTransition } from 'react-native-reanimated'
 
-import { type ProductType, type ReceiptType } from '@entities/receipt'
+import { type ProductType, type ReceiptType, receipts } from '@entities/receipt'
 
 import { useTheme } from '@shared/lib'
 
@@ -34,47 +36,49 @@ const StyledDashedLine = cssInterop(Dash, {
   },
 })
 
-export const Receipt: FC<ReceiptProps> = ({
-  receipt,
-  openModal,
-  closeModal,
-  selectProduct,
-}) => {
-  const { isDark } = useTheme()
+export const Receipt: FC<ReceiptProps> = observer(
+  ({ receipt, openModal, closeModal, selectProduct }) => {
+    const { isDark } = useTheme()
 
-  const renderItem = useCallback<ListRenderItem<ProductType>>(
-    ({ item }) => (
-      <Product
-        onPress={(product) => {
-          openModal()
-          selectProduct(product)
-        }}
-        receiptId={receipt.id}
-        product={item}
-      />
-    ),
-    [],
-  )
+    const renderItem = useCallback<ListRenderItem<ProductType>>(
+      ({ item }) => (
+        <Product
+          onPress={(product) => {
+            openModal()
+            selectProduct(product)
+          }}
+          onDelete={(id) => receipts.deleteProduct(receipt.id, id)}
+          receiptId={receipt.id}
+          product={item}
+        />
+      ),
+      [],
+    )
 
-  return (
-    <View className="mb-28 overflow-hidden">
-      <FlatList
-        data={receipt?.products}
-        ListHeaderComponent={
-          receipt ? (
-            <Header receipt={receipt} openModal={openModal} {...receipt} />
-          ) : null
-        }
-        stickyHeaderIndices={[0]}
-        keyExtractor={(item) => item.id}
-        className={`z-10 rounded-t-medium bg-default-200 ${isDark && '!bg-default-100'}`}
-        renderItem={renderItem}
-      />
-      <StyledDashedLine
-        dashClassName={`-translate-y-[7px] rotate-45 rounded-[2px] bg-default-200 ${isDark && '!bg-default-100'} z-0`}
-        dashLength={14}
-        dashThickness={14}
-      />
-    </View>
-  )
-}
+    return (
+      <View className="mb-28 flex-1">
+        <View className="max-h-full">
+          <Header receipt={receipt} openModal={openModal} {...receipt} />
+          <Animated.FlatList
+            data={receipt?.products}
+            keyExtractor={(item) => item.id}
+            className={`z-10 max-h-full bg-default-200 ${isDark && '!bg-default-100'}`}
+            renderItem={renderItem}
+            itemLayoutAnimation={LinearTransition}
+            layout={LinearTransition}
+          />
+          <Animated.View
+            className="w-full overflow-hidden"
+            layout={LinearTransition}
+          >
+            <StyledDashedLine
+              dashClassName={`-translate-y-[7px] rotate-45 rounded-[2px] bg-default-200 ${isDark && '!bg-default-100'} z-0`}
+              dashLength={14}
+              dashThickness={14}
+            />
+          </Animated.View>
+        </View>
+      </View>
+    )
+  },
+)
