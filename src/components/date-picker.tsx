ee9b065d21229ac56f9faaa07242bc5ea270aa41
Malberee <82259650+Malberee@users.noreply.github.com>
2025-08-27 18:13:55 +0300

    chore: update react-native-ui-datepicker to 3.1.2, direct use classNames instead cssInterop

--- src/components/date-picker.tsx
+++ src/components/date-picker.tsx
@@ -1,4 +1,3 @@
-import { cssInterop } from 'nativewind'
 import React, { type FC } from 'react'
 import { View } from 'react-native'
 import DateTimePicker from 'react-native-ui-datepicker'
@@ -8,48 +7,29 @@ interface DatePickerProps {
   setDate: (date: Date) => void
 }
 
-const StyledPicker = cssInterop(DateTimePicker, {
-  calendarTextClassname: 'calendarTextStyle',
-  headerTextClassname: 'headerTextStyle',
-  weekDaysTextClassName: 'weekDaysTextStyle',
-  headerButtonColorClassname: {
-    target: false,
-    nativeStyleToProp: {
-      color: 'headerButtonColor',
-    },
-  },
-  selectedItemColorClassName: {
-    target: false,
-    nativeStyleToProp: { color: 'selectedItemColor' },
-  },
-  todayContainerClassname: 'todayContainerStyle',
-  timePickerContainerClassname: 'timePickerContainerStyle',
-  timePickerTextClassname: 'timePickerTextStyle',
-  timePickerIndicatorClassname: 'timePickerIndicatorStyle',
-  monthContainerClassname: 'monthContainerStyle',
-  yearContainerClassname: 'yearContainerStyle',
-  weekDaysContainerClassname: 'weekDaysContainerStyle',
-})
-
 export const DatePicker: FC<DatePickerProps> = ({ date, setDate }) => {
   return (
-    <View className="rounded-medium bg-default-100">
-      <StyledPicker
+    <View className="rounded-xl bg-default-100">
+      <DateTimePicker
         mode="single"
         date={date}
         onChange={(params) => setDate(params.date as Date)}
+        classNames={{
+          time_label: 'text-foreground text-3xl',
+          time_selector_label: 'text-foreground',
+          year_label: 'text-foreground',
+          year_selector_label: 'text-foreground',
+          selected_year: 'bg-primary rounded-xl',
+          month_selector_label: 'text-foreground',
+          selected_month: 'bg-primary rounded-xl',
+          month_label: 'text-foreground',
+          weekday_label: 'text-primary-foreground bg-primary rounded-full px-2',
+          day_label: 'text-foreground',
+          selected: 'rounded-xl aspect-square !bg-primary',
+          selected_label: 'text-primary-foreground',
+          today: 'rounded-xl bg-default-300 dark:bg-default-200',
+        }}
         timePicker
-        calendarTextClassname="text-foreground"
-        headerTextClassname="text-foreground"
-        weekDaysTextClassName="text-foreground"
-        headerButtonColorClassname="text-foreground"
-        selectedItemColorClassName="text-primary"
-        todayContainerClassname="border-primary"
-        timePickerTextClassname="text-foreground"
-        timePickerIndicatorClassname="bg-default-100"
-        monthContainerClassname="bg-default-100 border-0"
-        yearContainerClassname="bg-default-100 border-0"
-        weekDaysContainerClassname="border-default-200"
       />
     </View>
   )
