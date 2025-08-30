c89158985ff51b5d05d6b1870bc3c3da8391c591
Malberee <82259650+Malberee@users.noreply.github.com>
2025-08-30 22:25:56 +0300

    fix(modal): replace KeyboardAvoidingView with KeyboardAwareScrollView

--- src/components/modal.tsx
+++ src/components/modal.tsx
@@ -6,15 +6,16 @@ import {
   BackHandler,
   Dimensions,
   Keyboard,
-  KeyboardAvoidingView,
   Pressable,
   View,
 } from 'react-native'
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
 import Animated, {
   FadeIn,
   FadeInDown,
   FadeOut,
   FadeOutDown,
+  LinearTransition,
 } from 'react-native-reanimated'
 
 interface ModalProps extends PropsWithChildren {
@@ -54,21 +55,29 @@ export const Modal: FC<ModalProps> = ({ children, onClose }) => {
   return (
     <Portal hostName="modal-portal">
       <View
-        className="absolute left-0 top-0 z-20 flex-row items-center justify-center"
+        className="absolute left-0 top-0 z-20"
         style={{ height: Dimensions.get('screen').height, width: '100%' }}
         onTouchStart={Keyboard.dismiss}
       >
-        <AnimatedPressable
-          className="absolute bottom-0 h-full w-full bg-black/70"
-          entering={FadeIn.duration(100)}
-          exiting={FadeOut.duration(100)}
-          onPress={onClose}
-        />
-        <KeyboardAvoidingView behavior="padding">
+        <KeyboardAwareScrollView
+          enableOnAndroid
+          contentContainerStyle={{
+            flex: 1,
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}
+        >
+          <AnimatedPressable
+            className="absolute top-0 h-full w-full bg-black/70"
+            entering={FadeIn.duration(100)}
+            exiting={FadeOut.duration(100)}
+            onPress={onClose}
+          />
           <Animated.View
             className="rounded-medium border border-default-100 bg-default-50 p-4 pt-10"
             entering={FadeInDown.duration(100)}
             exiting={FadeOutDown.duration(100)}
+            layout={LinearTransition.duration(200)}
           >
             <Button
               startContent={<Plus className="rotate-45 text-foreground" />}
@@ -80,7 +89,7 @@ export const Modal: FC<ModalProps> = ({ children, onClose }) => {
             />
             {children}
           </Animated.View>
-        </KeyboardAvoidingView>
+        </KeyboardAwareScrollView>
       </View>
     </Portal>
   )
